5802e016bd69222ca6a46e0ac3a74e9555bbf74a
William LeBorgne <williamleborgne@Williams-MacBook-Pro.local>
2016-02-13 17:19:03 -0700

    Static globals, constants, string interpolation

--- chapter_2/game/src/main.rs
+++ chapter_2/game/src/main.rs
@@ -1,3 +1,12 @@
+use std::f32::consts;
+
+// Global values!
+static MAX_HEALTH: i32 = 100;
+static GAME_NAME: &'static str = "Monster Attack";
+
 fn main() {
-    println!("Hello, world!");
+    // Game start: print a welcome message
+    println!("Welcome to {}!", GAME_NAME);
+    println!("You start with {} health points.", MAX_HEALTH);
+    println!("{}", consts::PI)
 }
